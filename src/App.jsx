c6867d8b115212ddb7c298467a9a3dba5b74953d
BuJassim <bn7ya@outlook.com>
2025-12-30 13:40:54 +0400

    feat: enhance styling and layout for improved UI experience

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,12 @@ import Counter from './Shared/Counter'
 
 
 function App() {
-  const titleStyle = { 
-    textAlign: 'right', 
-    background: 'linear-gradient(170deg, rgba(29, 92, 117, 1) 0%, rgba(87, 182, 199, 1) 50%, rgba(36, 180, 113, 1) 100%)',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-    margin: '5%', 
-    padding: '1%' 
-  }
-
-  const descStyle = { 
-    textAlign: 'left', 
-    background: 'linear-gradient(90deg, rgba(42, 138, 155, 1) 0%, rgba(100, 199, 87, 1) 50%, rgba(83, 237, 229, 1) 100%)',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-    margin: '5%', 
-    padding: '1%',
-  }
-
   const [count, setCount] = useState(0)
+
   return (
     <div className='first'>
-      <h1 style={titleStyle}>التطبيق الأول بـ React</h1>
-      <p style={descStyle}>العمل الذي سيكون افتتاحية لأفضل الواجهات في العالم بإذن الله</p>
+      <h1 className='title'>التطبيق الأول بـ React</h1>
+      <p className='description'>العمل الذي سيكون افتتاحية لأفضل الواجهات في العالم بإذن الله</p>
       <Button text={'اضغط'} onClick={() => setCount(count + 1)}></Button>
       <Counter number={count}/>
     </div>
